c0c15787b44cfd32d9bc4429b5672f18e711b385
FadelI <FadelI@users.noreply.github.com>
2023-10-12 16:03:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,23 +4,27 @@ It is based on the hrftn96 program (Computer programs in seismology; Herrmann, 2
 Th original program is written in Fortran and is available at http://www.eas.slu.edu/eqc/eqccps.html.
 rffw is the python module that compiles the Fotran code into a python lirary using f2py.
 
-
-
-
 # Installation
 Here are some instructions to install rf into a fresh conda environment::
+
 1. Deactivate running Conda Env
 conda deactivate
+
 2. Create new conda environment 'rffw' 
 conda create -n rffw python=3.9 anaconda -y
+
 3. Activate the environement 
 conda activate rffw
+
 4. Download via wget or web-browser
 wget https://github.com/akinremisa/rffw/archive/refs/heads/main.zip
+
 5. unzip the package
 unzip main.zip  
+
 6. Change directory to the same directory that this repo is in (i.e., same directory as setup.py)
-cd rffw-main  
+cd rffw-main 
+ 
 7. run pip comand:
 pip install - e. 
 
